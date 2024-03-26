5e86f5670f00d4f2ddf7ca65fe814fe69b990e61
adakamahesh <adakamahesh@gmail.com>
2024-03-26 12:30:59 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var username = [];
+var usernumber = [];
+var alluseres = [];
+var MLModel = [
+    [65, 25, 34], []
+];
+username.push("mahesh");
+usernumber.push(501);
+alluseres.push({ name: "jay", isactive: true });
+console.log(username);
+console.log(usernumber);
+console.log(alluseres);
+console.log(MLModel);
---
+++ arrays.ts
@@ -0,0 +1,25 @@
+const username:string[]=[]
+const usernumber:Array<number>=[]
+
+type user={
+    name:string
+    isactive:boolean
+}
+
+const alluseres:user[]=[]
+const MLModel:number[][]=[
+    [65,25,34],[]
+]
+
+username.push("mahesh")
+usernumber.push(501)
+alluseres.push({name:"jay",isactive:true})
+
+console.log(username);
+console.log(usernumber);
+console.log(alluseres);
+console.log(MLModel);
+
+
+
+export{}
