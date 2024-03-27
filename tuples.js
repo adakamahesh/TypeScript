933f9c05fa349437fd2e133d5d0e2ca3bc2ad02a
adakamahesh <adakamahesh@gmail.com>
2024-03-27 12:06:11 +0530

    tuples

---
+++ tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user; // Declaring a tuple 'user' with elements of type string, number, and boolean
+user = ['mahesh', 501, true]; // Assigning values to the 'user' tuple
+console.log(user);
+var rgb = [23, 32, 52]; // Declaring a tuple 'rgb' with elements of type number, number, and number
+var newuser = [122, 'mahesh']; // Declaring a 'newuser' of type 'user' and assigning it a tuple value
---
+++ tuples.ts
@@ -0,0 +1,11 @@
+let user:[string,number,boolean]// Declaring a tuple 'user' with elements of type string, number, and boolean
+user=['mahesh',501,true]// Assigning values to the 'user' tuple
+console.log(user);
+
+let rgb:[number,number,number]=[23,32,52]// Declaring a tuple 'rgb' with elements of type number, number, and number
+
+type user=[number,string]// Defining a type 'user' as a tuple with elements of type number and string
+const newuser:user=[122,'mahesh']// Declaring a 'newuser' of type 'user' and assigning it a tuple value
+
+
+export{}
