fa8237546901d9d74e578d156dd94d6b4c29fc91
adakamahesh <adakamahesh@gmail.com>
2024-03-27 11:59:13 +0530

    unions

--- union.js
+++ union.js
@@ -1,12 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var score;
-score = 45;
-score = 'ten';
+var score; // Declaring a variable 'score' with type number|string
+score = 45; // Assigning a number to 'score'
+score = 'ten'; // Assigning a string to 'score'
+// Declaring a variable 'amb' with type annotation user|Admin and assigning it an object of type user
 var amb = { name: 'amb', id: 501 };
-amb = { username: 'amb', id: 501 };
+amb = { username: 'amb', id: 501 }; // Reassigning 'amb' with an object of type Admin
+// Defining a function 'getdbid' which takes a parameter 'id' of type number|string and logs a message
 function getdbid(id) {
     console.log("db id is:".concat(id));
 }
-getdbid(5);
-getdbid('5');
+getdbid(5); // the 'getdbid' function with a number argument
+getdbid('5'); // the 'getdbid' function with a string argument
--- union.ts
+++ union.ts
@@ -1,23 +1,27 @@
-let score:number|string
-score=45
-score='ten'
+let score:number|string// Declaring a variable 'score' with type number|string
+score=45// Assigning a number to 'score'
+score='ten'// Assigning a string to 'score'
 
+// Defining a type 'user' with properties 'name' (string) and 'id' (number)
 type user={
     name:string;
     id:number
 }
 
+// Defining a type 'Admin' with properties 'username' (string) and 'id' (number)
 type Admin={
     username:string;
     id:number
 }
 
+// Declaring a variable 'amb' with type annotation user|Admin and assigning it an object of type user
 let amb:user|Admin={name:'amb',id:501}
-amb={username:'amb',id:501}
+amb={username:'amb',id:501}// Reassigning 'amb' with an object of type Admin
 
+// Defining a function 'getdbid' which takes a parameter 'id' of type number|string and logs a message
 function getdbid(id:number|string){
     console.log(`db id is:${id}`);   
 }
-getdbid(5)
-getdbid('5')
+getdbid(5)// the 'getdbid' function with a number argument
+getdbid('5')// the 'getdbid' function with a string argument
 export{}
