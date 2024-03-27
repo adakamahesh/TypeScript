6fd44e7c358b49285ce37a3f65d82ec903366b3c
adakamahesh <adakamahesh@gmail.com>
2024-03-27 11:34:11 +0530

    arrays

--- arrays.js
+++ arrays.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var username = [];
-var usernumber = [];
-var alluseres = [];
+var username = []; // Declaring an empty array of strings to store usernames
+var usernumber = []; // Declaring an empty array of numbers using a different syntax
+var alluseres = []; // Declaring an empty array of objects of type 'user' to store all users
 var MLModel = [
     [65, 25, 34], []
-];
-username.push("mahesh");
-usernumber.push(501);
-alluseres.push({ name: "jay", isactive: true });
+]; // Declaring a 2D array to store numerical data for MLModel
+username.push("mahesh"); // Adding a username to the 'username' array
+usernumber.push(501); // Adding a number to the 'usernumber' array
+alluseres.push({ name: "jay", isactive: true }); // Adding a user object to the 'alluseres' array
 console.log(username);
 console.log(usernumber);
 console.log(alluseres);
--- arrays.ts
+++ arrays.ts
@@ -1,25 +1,24 @@
-const username:string[]=[]
-const usernumber:Array<number>=[]
+const username:string[]=[]// Declaring an empty array of strings to store usernames
+const usernumber:Array<number>=[]// Declaring an empty array of numbers using a different syntax
 
+// Defining a type 'user' with properties 'name' and 'isactive'
 type user={
     name:string
     isactive:boolean
 }
 
-const alluseres:user[]=[]
+const alluseres:user[]=[]// Declaring an empty array of objects of type 'user' to store all users
 const MLModel:number[][]=[
     [65,25,34],[]
-]
+]// Declaring a 2D array to store numerical data for MLModel
 
-username.push("mahesh")
-usernumber.push(501)
-alluseres.push({name:"jay",isactive:true})
+username.push("mahesh")// Adding a username to the 'username' array
+usernumber.push(501)// Adding a number to the 'usernumber' array
+alluseres.push({name:"jay",isactive:true})// Adding a user object to the 'alluseres' array
 
 console.log(username);
 console.log(usernumber);
 console.log(alluseres);
 console.log(MLModel);
-
-
 
 export{}
