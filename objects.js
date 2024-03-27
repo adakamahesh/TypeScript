901041c2598245e4c939235c8448342325d5d630
adakamahesh <adakamahesh@gmail.com>
2024-03-27 11:49:02 +0530

    objects

--- objects.js
+++ objects.js
@@ -1,16 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Declaring an object 'user' with properties 'name', 'email', and 'isActive'
 var user = {
     name: 'mahesh',
     email: "mahesh@gmail.com",
     isActive: true
 };
+// Defining a function 'createuser' which takes an object as input with properties 'name' (string) and 'ispaid' (boolean)
 function creatuser(_a) {
     var string = _a.name, boolean = _a.ispaid;
 }
-var newuser = { name: "mahesh", ispaid: false };
+var newuser = { name: "mahesh", ispaid: false }; // Declaring an object 'newuser' with properties 'name' and 'ispaid'
 console.log(newuser);
 creatuser(newuser);
+// Defining a function 'createcourse' which returns an object with properties 'name' (string) and 'price' (number)
 function createcourse() {
     return { name: "sri", price: 399 };
 }
--- objects.ts
+++ objects.ts
@@ -1,14 +1,17 @@
+// Declaring an object 'user' with properties 'name', 'email', and 'isActive'
 const user={
     name:'mahesh',
     email:"mahesh@gmail.com",
     isActive:true
 }
 
+// Defining a function 'createuser' which takes an object as input with properties 'name' (string) and 'ispaid' (boolean)
 function creatuser({name:string, ispaid:boolean}){}
-let newuser={name:"mahesh",ispaid:false}
+let newuser={name:"mahesh",ispaid:false}// Declaring an object 'newuser' with properties 'name' and 'ispaid'
 console.log(newuser);
 creatuser(newuser);
 
+// Defining a function 'createcourse' which returns an object with properties 'name' (string) and 'price' (number)
 function createcourse():{name:string,price:number}{
     return {name:"sri", price:399}
 }
