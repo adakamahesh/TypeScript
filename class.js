1e88921d584431a409b885e1b0d71d85cf680726
adakamahesh <adakamahesh@gmail.com>
2024-03-27 17:46:58 +0530

    class

---
+++ class.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = /** @class */ (function () {
+    function user(email, name) {
+        this.email;
+        this.name;
+    } // Assigning the constructor parameters to the class properties
+    return user;
+}());
+var mahesh = new user("m@b", "mahesh"); // Creating a new instance of the 'user' class
+
---
+++ class.ts
@@ -0,0 +1,13 @@
+class user{
+    email:string
+    name:string
+    constructor(email:string,name:string){
+        this.email;
+        this.name
+    }// Assigning the constructor parameters to the class properties
+}
+
+const mahesh=new user("m@b","mahesh");// Creating a new instance of the 'user' class
+
+
+export{}
