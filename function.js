d209e57f74fbc8fe962312ec5abcda8a9cda3b37
adakamahesh <adakamahesh@gmail.com>
2024-03-27 11:41:27 +0530

    functions

--- function.js
+++ function.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Defining a function 'addtwo' which takes a number as input and returns the number plus 2
 function addtwo(num) {
     return num + 2;
 }
+//the 'addtwo' function with argument 5 and storing the result in 'myvalue'
 var myvalue = addtwo(5);
-console.log(myvalue);
+console.log(myvalue); //this is used to display the myvalue
--- function.ts
+++ function.ts
@@ -1,6 +1,8 @@
+// Defining a function 'addtwo' which takes a number as input and returns the number plus 2
 function addtwo(num:number) {
     return num+2;
 }
+//the 'addtwo' function with argument 5 and storing the result in 'myvalue'
 let myvalue=addtwo(5);
-console.log(myvalue);
+console.log(myvalue);//this is used to display the myvalue
 export{}
